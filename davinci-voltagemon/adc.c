fa361257987177c207519d806b90f8e495d00973
Alex Camilo <acamilo@Alexs-MacBook.local>
2015-06-06 14:02:03 -0400

    Now detects if attiny261 or attiny 26

--- davinci-voltagemon/adc.c
+++ davinci-voltagemon/adc.c
@@ -19,21 +19,37 @@ void initADC()
             (0 << MUX2)  |     // use ADC2 for input (PB4), MUX bit 2
             (0 << MUX1)  |     // use ADC2 for input (PB4), MUX bit 1
             (0 << MUX0);       // use ADC2 for input (PB4), MUX bit 0
-
+#ifdef __AVR_ATtiny261__
   ADCSRA = 
 	    (0 << ADATE) |     // auto trigger mode, off
             (1 << ADEN)  |     // Enable ADC 
             (0 << ADPS2) |     // set prescaler to 2, bit 2 
             (0 << ADPS1) |     // set prescaler to 2, bit 1 
             (0 << ADPS0);      // set prescaler to 2, bit 0  
+#else
+  ADCSR = 
+		(0 << ADFR) |     // auto trigger mode, off
+	        (1 << ADEN)  |     // Enable ADC 
+	        (0 << ADPS2) |     // set prescaler to 2, bit 2 
+	        (0 << ADPS1) |     // set prescaler to 2, bit 1 
+	        (0 << ADPS0);      // set prescaler to 2, bit 0
+#endif
 }
 
 unsigned long int getAdc(int channel){
 
 ADMUX =(ADMUX&0xe0)+(channel&0x1f); // Select channel
-ADCSRA |= (1 << ADSC);         // start ADC measurement
-while (ADCSRA & (1 << ADSC) ); // wait till conversion complete 
+#ifdef __AVR_ATtiny261__
+	ADCSRA |= (1 << ADSC);         // start ADC measurement
+#else
+	ADCSR |= (1 << ADSC);         // start ADC measurement
+#endif
 
+#ifdef __AVR_ATtiny261__
+	while (ADCSRA & (1 << ADSC) ); // wait till conversion complete 
+#else
+	while (ADCSR & (1 << ADSC) ); // wait till conversion complete 
+#endif
 return (ADCL>>6)+(ADCH<<2); // need to read ADCL FIRST otherwise ADC won't work. says DS.
 
 }
